3709a08ee6584e135143af3802f8a35fc17fea96
Mansoor <hussainmansoor876@gmail.com>
2019-12-04 12:57:06 +0500

    DOne

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 data = pd.read_excel('data.xlsx', usecols='B,C,D,E,F,G')
@@ -13,7 +13,8 @@ def index():
     return render_template('index.html')
 
 @app.route('/result', method=["POST"])
-def result()
+def result():
+    return "Hello"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
