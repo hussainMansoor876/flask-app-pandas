ccc07836e3f351af4e89da8775d2a8c104553885
Mansoor <hussainmansoor876@gmail.com>
2019-12-04 17:21:01 +0500

    Form done

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import random
 
 data = pd.read_excel('data.xlsx', usecols='B,C,D,E,F,G')
-data
+
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/result', methods=["POST"])
 def result():
-    return "Hello"
+    dict1 = {}
+    req = dict(request.form)
+    print(req)
+    for i in data:
+        ind = random.randint(0, int(len(data[i].dropna())-1))
+        dict1[i] = data[i][ind]
+    return jsonify({"result": dict1})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
