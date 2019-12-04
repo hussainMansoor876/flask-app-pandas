f49307d22e073d0119aa1c20f3814719af34fdc6
Mansoor <hussainmansoor876@gmail.com>
2019-12-04 18:58:33 +0500

    Done

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import random
 
-data = pd.read_excel('data.xlsx', usecols='B,C,D,E,F,G')
+data = pd.read_excel('data.xlsx', usecols='B,C,D,E,F,G') # Read data from excel
 dict1 = {}
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/') #Return the form
 def index():
     for i in data:
-        ind = random.randint(0, int(len(data[i].dropna())-1))
-        dict1[i] = data[i][ind]
+        ind = random.randint(0, int(len(data[i].dropna())-1)) #generate the random number between 0 and len of column
+        dict1[i] = data[i][ind] #assign the key and value base on random value
     return render_template('index.html')
 
 
 @app.route('/result', methods=["POST"])
 def result():
     req = dict(request.form)
-    print(req)
     return render_template('result.html', req=req, dict1=dict1, keys=data.columns)
 
 
